26c6a06d8965c7416112bc5e7367a75dac7bd457
TNHdev123 <TNHdev123@outlook.com>
2026-07-29 09:37:50 +0800

    Replace CameraView with SMSView for SMS case

--- Sources/SkankPhoneSwiftUIRetro/Main.swift
+++ Sources/SkankPhoneSwiftUIRetro/Main.swift
@@ -74,7 +74,7 @@ struct RootView: View {
                 case .camera:
                     CameraView(currentApp: $currentApp)
                 case .sms:
-                    CameraView(currentApp: $currentApp)
+                    SMSView(currentApp: $currentApp)
                 }
             }
             .ignoresSafeArea(edges: .top) // <- 全域生效，所有子畫面頂部都會貼緊螢幕最上方
